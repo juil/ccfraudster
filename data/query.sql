e9478c45dd9636fb3dd95449906e4f18111c6446
Juil <dev@juil.me>
2023-01-18 12:04:50 -0500

    Query CEO's credit card transactions from Jan-Jun 2018

--- data/query.sql
+++ data/query.sql
@@ -12,3 +12,11 @@ where cc.cardholder_id = 2 or cc.cardholder_id = 18;
 alter table "transaction" 
 alter column amount 
 type numeric using amount::numeric;
+
+-- Retrieve tx data from jan to jun 2018 for card holder 25
+select tx.*
+from "transaction" tx
+left join credit_card cc on cc.card=tx.card 
+where cc.cardholder_id = 25 and 
+	tx."date" >= '2018-01-01' and 
+	tx."date" < '2018-07-01';
