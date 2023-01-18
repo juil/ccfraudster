c609530ccab548bb784bef82daf7c0515a19a9cf
Juil <jiy.juil@gmail.com>
2023-01-18 15:37:18 -0500

    Create view: top 100 transactions made between 07:00-09:00

--- data_analysis.sql
+++ data_analysis.sql
@@ -14,3 +14,15 @@ group by cc.cardholder_id
 select * from less_than_2
 order by tx_lessthan2 desc;
 
+-- Create View: Top 100 highest transactions made between 07:00-09:00
+drop view between_7_9
+
+create view between_7_9 as
+select *
+from "transaction" tx
+where tx.date::time >= '07:00:00'::time
+	and tx.date::time < '09:00:00'::time
+order by amount desc 
+fetch first 100 rows only;
+	
+select * from between_7_9;
