29265da9fa244f5a10f8ebb2cac7ae1bb9a56af6
Juil <dev@juil.me>
2023-01-18 02:34:44 -0500

    Update query to left join credit card transactions.

---
+++ data/query.sql
@@ -0,0 +1,8 @@
+
+-- Get all transactions for user ID 2 & 18
+
+select cc.cardholder_id, tx.*
+from "transaction" tx
+left join credit_card cc ON cc.card=tx.card
+where cc.cardholder_id = 2 or cc.cardholder_id = 18;
+
